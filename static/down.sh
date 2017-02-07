1f3582a5405ef2376a51f1e16c211d62939487bb
Marco Hemken <mehemken@gmail.com>
2017-02-07 07:37:05 -0800

    Auto commit: Tue Feb  7 07:37:05 PST 2017

--- static/down.sh
+++ static/down.sh
@@ -11,3 +11,64 @@ cd ~/Documents/notes/
 docker-compose down
 
 tmux kill-session -t $SESSION_NAME
+
+
+##############################
+# Git stuff
+
+
+function set_to_branch_devel () {
+    if [ "$CURRENT_BRANCH" != "devel" ]; then
+        git checkout devel
+    fi
+}
+
+function reset_branch_devel () {
+    set_to_branch_devel
+    git reset
+}
+
+
+CURRENT_BRANCH=$(git symbolic-ref HEAD | sed 's!refs\/heads\/!!')
+set_to_branch_devel
+
+
+git add .
+git status
+echo "Are you sure? yes/no"
+read CHOICE
+
+if [ "$CHOICE" == "yes" ]; then
+    echo "Ok. You asked for it."
+    echo "building..."
+    TIMESTAMP="$(date)"
+    git commit -am "Auto commit: $TIMESTAMP"
+elif [ "$CHOICE" == "no" ]; then
+    echo "You said no."
+    echo "Build aborted."
+    git reset
+    exit $?
+else
+    echo "Please say yes/no."
+    echo "Build aborted."
+    exit $?
+fi
+
+echo "Merge? yes/no"
+read MERGE_CHOICE
+
+if [ "$MERGE_CHOICE" == "yes" ]; then
+    echo "Ok. You asked for it again."
+    echo "Merging..."
+    git checkout master
+    git merge devel
+    git checkout devel
+elif [ "$MERGE_CHOICE" == "no" ]; then
+    echo "You said no."
+    echo "Merge aborted."
+    exit $?
+else
+    echo "Please say yes/no."
+    echo "Merge aborted."
+    exit $?
+fi
